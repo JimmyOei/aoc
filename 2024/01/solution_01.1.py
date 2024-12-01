6c8a85f4521dd4e006f98f4f9973a271ec6f4ace
Oei, J.T. (Jimmy) <jimmy.oei@nn-group.com>
2024-12-01 06:10:22 +0100

    day 1

--- 2024/01/solution_01.1.py
+++ 2024/01/solution_01.1.py
@@ -1,1 +1,18 @@
 input = open('./input_01.txt', 'r')
+lines = input.readlines()
+
+list1 = []
+list2 = []
+
+for line in lines:
+    nums = line.split()
+    list1.append(int(nums[0]))
+    list2.append(int(nums[1]))
+
+list1.sort()
+list2.sort()
+
+ans = 0
+for i in range(len(list1)):
+    ans += abs(list1[i] - list2[i])
+print(ans)
--- 2024/01/solution_01.2.py
+++ 2024/01/solution_01.2.py
@@ -1,1 +1,19 @@
 input = open('./input_01.txt', 'r')
+lines = input.readlines()
+
+list1 = []
+list2 = []
+
+for line in lines:
+    nums = line.split()
+    list1.append(int(nums[0]))
+    list2.append(int(nums[1]))
+
+list1.sort()
+list2.sort()
+
+ans = 0
+for i in range(len(list1)):
+    cnt = list2.count(list1[i])
+    ans += (cnt*list1[i])
+print(ans)
