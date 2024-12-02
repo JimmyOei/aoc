332ea902c79d349af5908a6398eb4b60752ca8f5
Oei, J.T. (Jimmy) <jimmy.oei@nn-group.com>
2024-12-02 06:58:44 +0100

    day 2

--- 2024/02/solution_02.1.py
+++ 2024/02/solution_02.1.py
@@ -1,1 +1,24 @@
 input = open('./input_02.txt', 'r')
+lines = input.readlines()
+
+ans = 0
+for line in lines:
+  digits = line.split()
+  digits = [int(d) for d in digits]
+  prev = digits[0]
+  increasing = digits[0] < digits[-1]
+  safe = True
+  for digit in digits[1:]:
+    if abs(digit - prev) > 3 or abs(digit - prev) < 1:
+      safe = False
+      break
+    if digit < prev and increasing:
+      safe = False
+      break
+    if digit > prev and not increasing:
+      safe = False
+      break
+    prev = digit
+  if safe:
+    ans += 1
+print(ans)
--- 2024/02/solution_02.2.py
+++ 2024/02/solution_02.2.py
@@ -1,1 +1,41 @@
 input = open('./input_02.txt', 'r')
+lines = input.readlines()
+
+def isSafe(i, j, increasing):
+  if abs(i - j) > 3 or abs(i - j) < 1:
+    return False
+  if i < j and increasing:
+    return False
+  if i > j and not increasing:
+    return False
+  return True
+
+def isLevelSafe(digits, increasing):
+  prev = digits[0]
+  for digit in digits[1:]:
+    if not isSafe(digit, prev, increasing):
+      return False
+    prev = digit
+  return True
+
+ans = 0
+for line in lines:
+  digits = line.split()
+  digits = [int(d) for d in digits]
+
+  prev = digits[1]
+  sep = 0
+  for digit in digits[2:]:
+    sep += prev-digit
+    prev = digit
+  increasing = sep < 0
+
+  if isLevelSafe(digits, increasing):
+    ans += 1
+    continue
+  for i in range(len(digits)):
+    digitsWithoutI = digits[:i] + digits[i+1:]
+    if isLevelSafe(digitsWithoutI, increasing):
+      ans += 1
+      break
+print(ans)
