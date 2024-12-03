9d7538b727b953435035de7b2b330207c757d0b2
Oei, J.T. (Jimmy) <jimmy.oei@nn-group.com>
2024-12-03 06:20:27 +0100

    3

--- 2024/03/solution_03.1.py
+++ 2024/03/solution_03.1.py
@@ -1,1 +1,16 @@
+import re
+
+def mul(x, y):
+    return x * y
+
 input = open('./input_03.txt', 'r')
+lines = input.readlines()
+
+ans = 0
+for line in lines:
+  muls = re.findall(r'mul\(\d{1,3},\d{1,3}\)', line)
+  for m in muls:
+    x, y = map(int, re.findall(r'\d{1,3}', m))
+    ans += mul(x, y)
+
+print(ans)
--- 2024/03/solution_03.2.py
+++ 2024/03/solution_03.2.py
@@ -1,1 +1,23 @@
+import re
+
+def mul(x, y):
+    return x * y
+
 input = open('./input_03.txt', 'r')
+lines = input.readlines()
+
+do = True
+
+ans = 0
+for line in lines:
+  funcs = re.findall(r'mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)', line)
+  for f in funcs:
+    if f == 'do()':
+      do = True
+    elif f == 'don\'t()':
+      do = False
+    elif do:
+      x, y = map(int, re.findall(r'\d{1,3}', f))
+      ans += mul(x, y)
+
+print(ans)
