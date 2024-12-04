2c36a4b60b78a9b8a2e0217dfef2ea7544d496db
Oei, J.T. (Jimmy) <jimmy.oei@nn-group.com>
2024-12-04 07:00:00 +0100

    4

--- 2024/04/solution_04.1.py
+++ 2024/04/solution_04.1.py
@@ -1,1 +1,46 @@
 input = open('./input_04.txt', 'r')
+lines = input.readlines()
+lines = [list(line.strip()) for line in lines]
+
+n = len(lines)
+m = len(lines[0])
+ans = 0
+
+for line in lines:
+  strline = ''.join(line)
+  ans += strline.count("XMAS") + strline.count("SAMX")
+
+vertlines = list(map(list, zip(*lines)))
+for line in vertlines:
+  strline = ''.join(line)
+  ans += strline.count("XMAS") + strline.count("SAMX")
+
+rightdiagonallines = [[] for i in range(n*2)]
+for i in range(n):
+  for j in range(m):
+    index = i-j
+    if index < 0:
+      index = -index + n - 1
+    rightdiagonallines[index].append(lines[i][j])
+
+for line in rightdiagonallines:
+  strline = ''.join(line)
+  ans += strline.count("XMAS") + strline.count("SAMX")
+
+reversedlines = [line[::-1] for line in lines]
+leftdiagonallines = [[] for i in range(n*2)]
+for i in range(n):
+  for j in range(m):
+    index = i-j
+    if index < 0:
+      index = -index + len(reversedlines) - 1
+    leftdiagonallines[index].append(reversedlines[i][j])
+
+for line in leftdiagonallines:
+  strline = ''.join(line)
+  ans += strline.count("XMAS") + strline.count("SAMX")
+
+print(ans)
+
+
+
--- 2024/04/solution_04.2.py
+++ 2024/04/solution_04.2.py
@@ -1,1 +1,59 @@
+import re
+
 input = open('./input_04.txt', 'r')
+lines = input.readlines()
+lines = [list(line.strip()) for line in lines]
+
+n = len(lines)
+m = len(lines[0])
+mas = []
+ans = 0
+
+rightbotdiagonallines = [[] for i in range(n*2)]
+rightbotcoords = [[] for i in range(n*2)]
+for i in range(n):
+  for j in range(m):
+    index = i-j
+    if index < 0:
+      index = -index + n - 1
+    rightbotdiagonallines[index].append(lines[i][j])
+    rightbotcoords[index].append((i, j))
+
+reversedlines = [line[::-1] for line in lines]
+leftbotdiagonallines = [[] for i in range(n*2)]
+leftbotcoords = [[] for i in range(n*2)]
+for i in range(n):
+  for j in range(m):
+    index = i-j
+    if index < 0:
+      index = -index + len(reversedlines) - 1
+    leftbotdiagonallines[index].append(reversedlines[i][j])
+    leftbotcoords[index].append((i, m-j-1))
+
+
+for idx, line in enumerate(rightbotdiagonallines):
+  strline = ''.join(line)
+  find = [m.start() for m in re.finditer('MAS', strline)] + [m.start() for m in re.finditer('SAM', strline)]
+  for f in find:
+    coords = rightbotcoords[idx][f+1]
+    if coords in mas:
+      mas.remove(coords)
+      ans += 1
+    else:
+      mas.append(coords)
+
+for idx, line in enumerate(leftbotdiagonallines):
+  strline = ''.join(line)
+  find = [m.start() for m in re.finditer('MAS', strline)] + [m.start() for m in re.finditer('SAM', strline)]
+  for f in find:
+    coords = leftbotcoords[idx][f+1]
+    if coords in mas:
+      mas.remove(coords)
+      ans += 1
+    else:
+      mas.append(coords)
+
+print(ans)
+
+
+
