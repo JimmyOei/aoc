3ee9af710271c45179e5acdf7995d9775e847d1a
Oei, J.T. (Jimmy) <jimmy.oei@nn-group.com>
2024-12-07 07:11:47 +0100

    7

--- 2024/07/solution_07.2.py
+++ 2024/07/solution_07.2.py
@@ -1,1 +1,25 @@
 input = open('./input_07.txt', 'r')
+lines = input.readlines()
+lines = [line.strip() for line in lines]
+
+def combine(x, y):
+  num = str(x) + str(y)
+  return int(num)
+
+def is_valid(target, val, nums, i):
+  if i == len(nums):
+    return val == target
+  if val > target:
+    return False
+  return is_valid(target, val * nums[i], nums, i + 1) or is_valid(target, combine(val, nums[i]), nums, i + 1) or is_valid(target, val + nums[i], nums, i + 1)
+
+ans = 0
+for line in lines:
+  line = line.replace(':', '')
+  nums = line.split(' ')
+  nums = [int(num) for num in nums]
+  target = nums[0]
+  if is_valid(target, int(nums[1]), nums[2:], 0):
+    ans += target
+
+print(ans)
