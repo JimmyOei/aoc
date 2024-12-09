c01155cadace5ebff2ef76116d17cac8656b583e
Oei, J.T. (Jimmy) <jimmy.oei@nn-group.com>
2024-12-09 07:01:09 +0100

    9

--- 2024/09/solution_09.1.py
+++ 2024/09/solution_09.1.py
@@ -1,1 +1,47 @@
 input = open('./input_09.txt', 'r')
+lines = input.readlines()
+lines = [line.strip() for line in lines]
+disk = [int(x) for x in list(lines[0])]
+
+ans = 0
+i = 0
+k = 0
+idi = 0
+j = len(disk) - 1
+idj = (j // 2)
+
+while i < j:
+    if i % 2 == 0:
+        while disk[i] > 0:
+            ans += idi*k
+            disk[i] -= 1
+            k += 1
+        i += 1
+        idi += 1
+    else:
+        while disk[j] > 0 and disk[i] > 0:
+            ans += idj*k
+            disk[i] -= 1
+            disk[j] -= 1
+            k += 1
+        if disk[j] == 0:
+            j -= 2
+            idj -= 1
+        if disk[i] == 0:
+            i += 1
+
+while i < len(disk):
+    if i % 2 == 0:
+        while disk[i] > 0:
+            ans += idi*k
+            disk[i] -= 1
+            k += 1
+        i += 1
+        idi += 1
+    else:
+        while disk[i] > 0:
+            k += 1
+            disk[i] -= 1
+        i += 1
+print(ans)
+        
--- 2024/09/solution_09.2.py
+++ 2024/09/solution_09.2.py
@@ -1,1 +1,55 @@
 input = open('./input_09.txt', 'r')
+lines = input.readlines()
+lines = [line.strip() for line in lines]
+disk = [int(x) for x in list(lines[0])]
+
+n = len(disk)
+
+gaps = [0] * n
+ids = [0] * n
+cnts = [0] * n
+
+idi = 0
+for i in range(n):
+  if i % 2 != 0:
+    gaps[i] = disk[i]
+  else:
+    ids[i] = idi
+    cnts[i] = disk[i]
+    idi += 1
+
+j = n - 1
+while j > 0:
+  if cnts[j] == 0:
+    j -= 1
+    continue
+  i = 0
+  jcnt = cnts[j]
+  while i < n and i < j:
+    if gaps[i] >= jcnt:
+      if gaps[i] > jcnt:
+        gaps.insert(i + 1, gaps[i] - jcnt)
+        ids.insert(i + 1, 0)
+        cnts.insert(i + 1, 0)
+        j += 1
+      gaps[i] = 0
+      cnts[j] = 0
+      ids[i] = ids[j]
+      ids[j] = 0
+      cnts[i] = jcnt
+      gaps[j] = jcnt
+      break
+    i += 1
+  j -= 1
+
+ans = 0
+k = 0
+for i in range(n):
+  if ids[i] == 0:
+    k += gaps[i]
+  for l in range(cnts[i]):
+    ans += ids[i] * k
+    k += 1
+
+print(ans)
+        
