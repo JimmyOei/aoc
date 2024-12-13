222408299f6eb38993bd35a4bdc8b4135f73b761
Oei, J.T. (Jimmy) <jimmy.oei@nn-group.com>
2024-12-13 09:57:07 +0100

    13

--- 2024/13/solution_13.1.py
+++ 2024/13/solution_13.1.py
@@ -1,1 +1,45 @@
 input = open('./input_13.txt', 'r')
+lines = input.readlines()
+lines = [line.strip().split(' ') for line in lines]
+
+m = [[]]
+for line in lines:
+  if line == ['']:
+    m.append([])
+    continue
+  if len(line) == 3:
+    m[-1].append((int(line[1][2:-1]), int(line[2][2:])))
+  else:
+    m[-1].append(([int(line[2][2:-1]), int(line[3][2:])]))
+
+def find_closest(x, y, px, py):
+  closest = min(px // x, py // y)
+  return closest, px - x * closest, py - y * closest
+
+ans = 0
+for l in m:
+  ax, ay = l[0]
+  bx, by = l[1]
+  px, py = l[2]
+  
+  cb, cbx, cby = find_closest(bx, by, px, py)
+  ca, cax, cay = find_closest(ax, ay, cbx, cby)
+  if cax == 0 and cay == 0:
+    lowestcost = ca * 3 + cb
+
+  i = cb
+  lowestcost = float("inf")
+  prevx, prevy = cax, cay
+  while i > 0:
+    cy = py - i * by
+    cx = px - i * bx
+    ma, max, may = find_closest(ax, ay, cx, cy)
+    if max == 0 and may == 0:
+      cost = i + ma * 3
+      if cost < lowestcost:
+        lowestcost = cost
+    i -= 1
+  if lowestcost != float("inf"):
+    ans += lowestcost
+
+print(ans)
--- 2024/13/solution_13.2.py
+++ 2024/13/solution_13.2.py
@@ -1,1 +1,38 @@
+import sympy as sp
+
 input = open('./input_13.txt', 'r')
+lines = input.readlines()
+lines = [line.strip().split(' ') for line in lines]
+
+plus = 10000000000000
+m = [[]]
+for line in lines:
+  if line == ['']:
+    m.append([])
+    continue
+  if len(line) == 3:
+    m[-1].append((int(line[1][2:-1]) + plus , int(line[2][2:]) + plus))
+  else:
+    m[-1].append(([int(line[2][2:-1]), int(line[3][2:])]))
+
+def find_closest(x, y, px, py):
+  closest = min(px // x, py // y)
+  return closest, px - x * closest, py - y * closest
+
+ans = 0
+for l in m:
+  ax, ay = l[0]
+  bx, by = l[1]
+  px, py = l[2]
+
+  A, B = sp.symbols('A B')
+  eq1 = sp.Eq(A * ax + B * bx, px)
+  eq2 = sp.Eq(A * ay + B * by, py)
+
+  sol = sp.solve((eq1, eq2), (A, B))
+
+  if sol[A] > 0 and sol[B] > 0 and sol[A].is_integer and sol[B].is_integer:
+    ans += sol[A] * 3 + sol[B]
+
+
+print(ans)
