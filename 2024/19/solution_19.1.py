94c1d56f175baf5a15462d54e2bed6e9d6902721
Oei, J.T. (Jimmy) <jimmy.oei@nn-group.com>
2024-12-19 06:51:09 +0100

    19

--- 2024/19/solution_19.1.py
+++ 2024/19/solution_19.1.py
@@ -1,1 +1,45 @@
 input = open('./input_19.txt', 'r')
+lines = input.readlines()
+lines = [line.strip() for line in lines]
+
+patterns = []
+towels = []
+
+idx = 0
+for line in lines:
+    if line == '\n' or line == '':
+        break
+    ps = line.split(', ')
+    for p in ps:
+      patterns.append(p)
+    idx += 1
+
+for line in lines[idx+1:]:
+    towels.append(line)
+    
+memo = {}
+def composable(towel):
+    if towel in memo:
+        return memo[towel]
+      
+    n = len(towel)
+    if n == 0:
+        return True
+      
+    memo[towel] = False
+    for p in patterns:
+        m = len(p)
+        if m > n:
+            continue
+        if towel[:m] == p:
+            if composable(towel[m:]):
+                memo[towel] = True
+                break
+    return memo[towel]
+
+ans = 0
+for towel in towels:
+    if composable(towel):
+        ans += 1
+
+print(ans)
