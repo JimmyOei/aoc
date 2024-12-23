6febac61b42e4fda2fa54c106da034a919ff8f90
Oei, J.T. (Jimmy) <jimmy.oei10@gmail.com>
2024-12-23 10:46:10 +0100

    23

--- 2024/23/solution_23.1.py
+++ 2024/23/solution_23.1.py
@@ -1,1 +1,36 @@
 input = open('./input_23.txt', 'r')
+lines = input.readlines()
+lines = [line.strip() for line in lines]
+
+mp = {}
+
+for line in lines:
+  nodes = line.split('-')
+  if nodes[0] in mp:
+    mp[nodes[0]].append(nodes[1])
+  else:
+    mp[nodes[0]] = [nodes[1]]
+  if nodes[1] in mp:
+    mp[nodes[1]].append(nodes[0])
+  else:
+    mp[nodes[1]] = [nodes[0]]
+
+setlength = 3
+validsets = []
+for k in mp.keys():
+  if k.startswith('t'):
+    q = [(k, [k])]
+    for _ in range(setlength-1):
+      qlength = len(q)
+      for _ in range(qlength):
+        node, path = q.pop(0)
+        for neighbor in mp[node]:
+          if neighbor not in path:
+            q.append((neighbor, path + [neighbor]))
+    for node, path in q:
+      neighbors = mp[node]
+      path.sort()
+      if k in neighbors and path not in validsets:
+        validsets.append(path)
+ans = len(validsets)
+print(ans)
--- 2024/23/solution_23.2.py
+++ 2024/23/solution_23.2.py
@@ -1,1 +1,35 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
 input = open('./input_23.txt', 'r')
+lines = input.readlines()
+lines = [line.strip() for line in lines]
+
+mp = {}
+
+for line in lines:
+  nodes = line.split('-')
+  if nodes[0] in mp:
+    mp[nodes[0]].append(nodes[1])
+  else:
+    mp[nodes[0]] = [nodes[1]]
+  if nodes[1] in mp:
+    mp[nodes[1]].append(nodes[0])
+  else:
+    mp[nodes[1]] = [nodes[0]]
+    
+G = nx.Graph()
+for k in mp.keys():
+  for neighbor in mp[k]:
+    G.add_edge(k, neighbor)
+nx.draw(G, with_labels=True)
+plt.show()
+
+# ans = ["ob", "ji", "dy", "ka", "qv", "ry", "xz", "ef", "jv", "ua", "wt", "iw", "cw"]
+# >>> ans
+# ['ob', 'ji', 'dy', 'ka', 'qv', 'ry', 'xz', 'ef', 'jv', 'ua', 'wt', 'iw', 'cw']
+# >>> ans.sort()
+# >>> ans
+# ['cw', 'dy', 'ef', 'iw', 'ji', 'jv', 'ka', 'ob', 'qv', 'ry', 'ua', 'wt', 'xz']
+# >>> ",".join(ans)
+# 'cw,dy,ef,iw,ji,jv,ka,ob,qv,ry,ua,wt,xz'
