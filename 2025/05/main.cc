ba216e4bc215759c6c67a2d090d865ad6059253b
Jimmy <72915212+JimmyOei@users.noreply.github.com>
2025-12-07 21:26:30 +0100

    wokring on 5

--- 2025/05/main.cc
+++ 2025/05/main.cc
@@ -27,6 +27,52 @@ vector<string> getInputLines() {
 
 void part1(const vector<string>& lines) {
   int res = 0;
+  vector<vector<int>> ranges;
+  
+  // process ranges
+  int i = 0;
+  for (; i < lines.size(); ++i) {
+    cout << i << " " << line << endl;
+    if (line[i] == "") {
+      break;
+    }
+
+    int dashFind = line.find('-');
+    long start = stol(line.substr(0, dashFind));
+    long end = stol(line.substr(dashFind+1));
+
+    for (int j = 0; j < ranges.size(); ++j) {
+      if (start > ranges[j][1]) {
+        continue;
+      }
+
+      if (start < ranges[j][0]) {
+        if (end >= ranges[j][0]) {
+          ranges[j][0] = start;
+          ranges[j][1] = max(ranges[j][1], end);
+        }
+        else {
+          ranges.insert(j, {start, end});
+        }
+        break;
+      }
+
+      if (end <= ranges[j][1]) {
+        break;
+      }
+      
+      start = ranges[j][0];
+      while(j < ranges.size()-1 && end > ranges[j][1]) {
+        ranges.erase(ranges.begin()+j);
+      }
+      ranges[j][0] = start;
+      ranges[j][1] = max(ranges[j][1], end);
+    }
+
+    for (const auto& v : ranges) {
+      cout << v[0] << "-" << v[1] << endl;
+    }
+  }
 
   cout << "Part 1: " << res << endl;
 }
